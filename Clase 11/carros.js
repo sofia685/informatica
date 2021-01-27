ddf0af21e0020bf1efb73db93dab62b74b9bd81c
Sofía <sofisar2017@gmail.com>
2021-01-26 22:35:24 -0500

    vehiculos

--- Clase 11/carros.js
+++ Clase 11/carros.js
@@ -1,3 +1,6 @@
-let vehículo = {Marca:"Nissan", Modelo:"Leaf", Year:2010, kilometraje:325km, KPG:186.684}
-let vehículo2 = {Marca: "Ford", Modelo:"Fiesta Titanium", Year:2012, kilometraje:92078km, KPG:46.671 }
-let vehículo3 = {Marca: "Lancia", Modelo:"Voyager", Year:2011, kilometraje:600km, KPG:46.671, KPG }
+let vehículo1 = {Marca:"Nissan", Modelo:"Leaf", Year:2010, kilometraje:325, KPG:186.684}
+let vehículo2 = {Marca: "Ford", Modelo:"Fiesta Titanium", Year:2012, kilometraje:92078, KPG:46.671 }
+let vehículo3 = {Marca: "Lancia", Modelo:"Voyager", Year:2011, kilometraje:600, KPG:46.671, KPG }
+let vehículo4 = {Marca: "Porsche", Modelo:"Cayenne 2020", Year:2020, kilometraje:0, KPG:46.671, KPG:0.0804672 }
+let vehículos = [vehículo1, vehículo2, vehículo3, vehículo4]
+let conductor = {Nombre: "Alejandra", Edad: 22, Ciudad: "Barranquilla", Vehículo: Vehículo}
